87c98a72206a9fd1dc12ee401fe076f6972fd4e5
Twigz101 <alexcarlj@aol.com>
2022-12-08 21:25:15 -0800

    Still not complete

--- AdventOfCode2022_Challenge8/AdventOfCode2022_Challenge8/main.cpp
+++ AdventOfCode2022_Challenge8/AdventOfCode2022_Challenge8/main.cpp
@@ -22,7 +22,7 @@ int main()
 			for (char c : textLine)
 			{
 				rowList.push_back(int(c) - 48);
-				++row;
+				row = textLine.size();
 			}
 
 			treeGrid.push_back(rowList);
@@ -41,61 +41,52 @@ int main()
 	{
 		for (int y = 0; y < col; ++y)
 		{
-			std::cout << treeGrid[x][y];
+			int visibleDir = 0;
 
-			//int currentObstruct = 0;
-			//int dist = 1;
+			// Check Right
+			for (int offset = 0; offset < x; ++offset)
+			{
+				if (treeGrid[y][offset] >= treeGrid[y][x])
+				{
+					++visibleDir;
+					break;
+				}
+			}
 
-			//if (x != 0 && y != 0)
-			//{
-			//	//Check Up
-			//	while ((y - dist) < 0)
-			//	{
-			//		currentObstruct += treeGrid[x][y - dist];
-			//	}
+			// Check Left
+			for (int offset = row - 1; offset > x; --offset)
+			{
+				if (treeGrid[y][offset] >= treeGrid[y][x])
+				{
+					++visibleDir;
+					break;
+				}
+			}
 
-			//	if (currentObstruct < treeGrid[x][y])
-			//	{
-			//		++visibleTrees;
-			//		continue;
-			//	}
+			// Check Down
+			for (int offset = 0; offset < y; ++offset)
+			{
+				if (treeGrid[offset][x] >= treeGrid[y][x])
+				{
+					++visibleDir;
+				}
+			}
 
-			//	//Check Down
-			//	while ((y - dist) > col)
-			//	{
-			//		currentObstruct += treeGrid[x][y + dist];
-			//	}
+			// Check Up
+			for (int offset = col - 1; offset > y; --offset)
+			{
+				if (treeGrid[offset][x] >= treeGrid[y][x])
+				{
+					++visibleDir;
+				}
+			}
 
-			//	if (currentObstruct < treeGrid[x][y])
-			//	{
-			//		++visibleTrees;
-			//		continue;
-			//	}
-			//	
-			//	//Check right
-			//	while ((x - dist) < 0)
-			//	{
-			//		currentObstruct += treeGrid[x - dist][y];
-			//	}
 
-			//	if (currentObstruct < treeGrid[x][y])
-			//	{
-			//		++visibleTrees;
-			//		continue;
-			//	}
+			if (visibleDir > 0)
+			{
+				visibleTrees += visibleDir;
+			}
 
-			//	//Check Left
-			//	while ((x - dist) > row)
-			//	{
-			//		currentObstruct += treeGrid[x + dist][y];
-			//	}
-
-			//	if (currentObstruct < treeGrid[x][y])
-			//	{
-			//		++visibleTrees;
-			//		continue;
-			//	}
-			//}
 		}
 
 		std::cout << std::endl;
